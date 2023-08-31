06296c8a1f7f2500b6313ba54b4925e3999968e9
kimgeuntae <kok617@naver.com>
2023-08-31 17:17:40 +0900

    Ref: 판넬 함수로 변경

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -28,102 +28,113 @@ class _DetailScreenState extends State<DetailScreen> {
 
   Widget makeMenuButton() => Container();
 
+  Stack makeTopPanel() {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/${widget.movie.poster}'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          // 이미지 뒷 배경 불투명
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.black.withOpacity(0.1),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                        child: Image.asset(
+                          'images/${widget.movie.poster}',
+                          height: 300,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(7),
+                        child: Text(
+                          '99% 일치 2019 15+ 시즌 1개',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(7),
+                        child: Text(
+                          widget.movie.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(3),
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.red,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.play_arrow),
+                              Text('재생'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        child: Text('영화 설명'),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '출연: 현빈, 손예진, 서지혜\n제작자: 이정효, 박지은',
+                          style: TextStyle(
+                            color: Colors.white60,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        // 포지션으로 앱바 생성
+        Positioned(
+          child: AppBar(
+            backgroundColor: Colors.transparent, // 투명
+            elevation: 0,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView(
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/${widget.movie.poster}'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                // 이미지 뒷 배경 불투명
-                child: ClipRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.black.withOpacity(0.1),
-                      child: Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
-                              child: Image.asset(
-                                'images/${widget.movie.poster}',
-                                height: 300,
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(7),
-                              child: Text(
-                                '99% 일치 2019 15+ 시즌 1개',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 13),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(7),
-                              child: Text(
-                                widget.movie.title,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(3),
-                              child: TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  foregroundColor: Colors.white,
-                                  backgroundColor: Colors.red,
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(Icons.play_arrow),
-                                    Text('재생'),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(5),
-                              child: Text('영화 설명'),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(5),
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                '출연: 현빈, 손예진, 서지혜\n제작자: 이정효, 박지은',
-                                style: TextStyle(
-                                  color: Colors.white60,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          makeMenuButton(),
-        ],
-      )),
+        child: ListView(
+          children: [
+            makeTopPanel(),
+          ],
+        ),
+      ),
     );
   }
 }
